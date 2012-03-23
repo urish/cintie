ff2b7449f80508cfb01cc655ff8e0ad6be061f6a
Uri Shaked <uri@salsa4fun.co.il>
2012-03-23 17:26:40 +0200

    Implement engine start and stop

--- src/main/scala/org/urish/cintie/engine/CintieEngine.scala
+++ src/main/scala/org/urish/cintie/engine/CintieEngine.scala
@@ -5,16 +5,18 @@ import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.FloatControl
 
 class CintieEngine {
-  val baseDir = new File("""C:\projects\FreshPaint\Assaf""")
+  val baseDir = new File("""C:\p\fp\cintie\sound""")
   val vstPath = new File("""C:\Program Files (x86)\VstPlugins""")
 
   var players: List[Player] = List(new FourSourcePlayer(new File(baseDir, "gtr")), new FourSourcePlayer(new File(baseDir, "piano")),
     new FourSourcePlayer(new File(baseDir, "drum")), new VstHarmonicPlayer(new File(vstPath, "nexus.dll")))
 
   def start {
+    players.foreach(p => p.start)
   }
   
   def stop {
+    players.foreach(p => p.stop)
   }
   
   def player(id: Int) = players(id)
--- src/main/scala/org/urish/cintie/engine/Player.scala
+++ src/main/scala/org/urish/cintie/engine/Player.scala
@@ -10,15 +10,22 @@ import com.synthbot.audioio.vst.JVstAudioThread
 import org.urish.cintie.util.Library
 import org.urish.cintie.util.VstPresetLoader
 
-class Player {
+abstract class Player {
   var x: Float = 0;
   var y: Float = 0;
+
+  def start()
+  def stop()
 }
 
 class FourSourcePlayer(soundBank: File) extends Player {
   val clips = List(1, 2, 3, 4).map(i => new AudioClip(new File(soundBank, i + ".wav")))
   clips.map(clip => clip.start())
   update
+
+  def start() = clips.foreach(clip => clip.start)
+
+  def stop() = clips.foreach(clip => clip.stop)
 
   def setGain(i: Int, value: Float) {
     clips(i - 1).setVolume(value)
@@ -40,6 +47,10 @@ class VstHarmonicPlayer(vstPath: File) extends Player with Runnable {
   val vst = JVstHost2.newInstance(vstPath, SAMPLE_RATE, BLOCK_SIZE)
   VstPresetLoader.loadVstPreset(vst, new File(vstPath.getParent(), "nexus content/presets/piano/PN Nexus Grandpiano.fxp"))
   val audioThread = new JVstAudioThread(vst);
+  var playing: Boolean = false;
+
+  def start() = playing = true
+  def stop() = playing = false
 
   val thread = new Thread(audioThread);
   thread.setDaemon(true)
@@ -49,18 +60,22 @@ class VstHarmonicPlayer(vstPath: File) extends Player with Runnable {
   def run() {
     try {
       while (true) {
-        val note = (((this.x + this.y) * 24) + 48).asInstanceOf[Int];
+        if (!playing) {
+          Thread.sleep(100)
+        } else {
+          val note = (((this.x + this.y) * 24) + 48).asInstanceOf[Int];
 
-        val channel = 0
-        val velocity = 127
-        val messageOn = new ShortMessage();
-        messageOn.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
-        vst.queueMidiMessage(messageOn);
+          val channel = 0
+          val velocity = 127
+          val messageOn = new ShortMessage();
+          messageOn.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+          vst.queueMidiMessage(messageOn);
 
-        Thread.sleep(500);
-        val messgeOff = new ShortMessage();
-        messgeOff.setMessage(ShortMessage.NOTE_ON, channel, note, 0);
-        vst.queueMidiMessage(messgeOff);
+          Thread.sleep(500);
+          val messgeOff = new ShortMessage();
+          messgeOff.setMessage(ShortMessage.NOTE_ON, channel, note, 0);
+          vst.queueMidiMessage(messgeOff);
+        }
       }
     }
   }
