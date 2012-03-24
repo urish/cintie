d4fd7900fcfa37c725c4f5cbf8b2cfac7abee821
Uri Shaked <uri@salsa4fun.co.il>
2012-03-24 11:59:04 +0200

    Auto detect the 4-source players in the sound directory

--- src/main/scala/org/urish/cintie/engine/CintieEngine.scala
+++ src/main/scala/org/urish/cintie/engine/CintieEngine.scala
@@ -1,24 +1,30 @@
 package org.urish.cintie.engine
 import java.io.File
-import javax.sound.sampled.AudioInputStream
-import javax.sound.sampled.AudioSystem
-import javax.sound.sampled.FloatControl
+
 import scala.collection.mutable.ListBuffer
 
 class CintieEngine {
   val baseDir = new File(System.getProperty("cintie.soundDir"))
   val vstPath = new File("""C:\Program Files (x86)\VstPlugins""")
+  var players: Seq[Player] = loadPlayers()
 
-  var players: ListBuffer[Player] = ListBuffer(new FourSourcePlayer(new File(baseDir, "gtr")), new FourSourcePlayer(new File(baseDir, "piano")),
-    new FourSourcePlayer(new File(baseDir, "drum")))
-
-  try {
-    players += new VstHarmonicPlayer(new File(vstPath, "nexus.dll"))
-  } catch {
-    case e => {
-      e.printStackTrace()
-      System.err.println("WARN: Could not initialize VST engine; VST pawn will not be available")
+  def loadPlayers(): List[Player] = {
+    val result = new ListBuffer[Player]
+    
+    for (file <- baseDir.listFiles()) {
+    	result += new FourSourcePlayer(file)
     }
+
+    try {
+      result += new VstHarmonicPlayer(new File(vstPath, "nexus.dll"))
+    } catch {
+      case e => {
+        e.printStackTrace()
+        System.err.println("WARN: Could not initialize VST engine; VST pawn will not be available")
+      }
+    }
+    System.out.println(result)
+    return result.toList
   }
 
   def start {
