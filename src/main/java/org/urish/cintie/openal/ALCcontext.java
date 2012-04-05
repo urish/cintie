3a5a79e272881e899d75c0b78bd0a11d29d3eb26
Uri Shaked <uri@salsa4fun.co.il>
2012-04-06 02:19:28 +0300

    Cintie: Added a basic JNA wrapper around OpenAL API

---
+++ src/main/java/org/urish/cintie/openal/ALCcontext.java
@@ -0,0 +1,8 @@
+package org.urish.cintie.openal;
+
+import com.sun.jna.Structure;
+
+
+public class ALCcontext extends Structure implements Structure.ByReference {
+	public byte dummy;
+}
---
+++ src/main/java/org/urish/cintie/openal/ALCdevice.java
@@ -0,0 +1,7 @@
+package org.urish.cintie.openal;
+
+import com.sun.jna.Structure;
+
+public class ALCdevice extends Structure implements Structure.ByReference {
+	public byte dummy;
+}
