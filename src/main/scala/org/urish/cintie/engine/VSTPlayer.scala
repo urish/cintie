fbe0f32f98626cf454d564f3fae2bb7b4ffd4f58
Uri Shaked <uri@salsa4fun.co.il>
2012-05-04 20:19:23 +0300

    Make additional aspects of the VST Player configurable

--- src/main/scala/org/urish/cintie/engine/VSTPlayer.scala
+++ src/main/scala/org/urish/cintie/engine/VSTPlayer.scala
@@ -3,6 +3,8 @@ import java.io.File
 import java.io.FileInputStream
 import java.util.Properties
 
+import scala.collection.mutable.ListBuffer
+
 import org.urish.cintie.util.LibraryLoader
 import org.urish.openal.OpenAL
 
@@ -18,6 +20,10 @@ object NexusControlMapping {
   val output = 13
 }
 
+object VstPlayer {
+  var initialized = false
+}
+
 class VstPlayer(openAL: OpenAL, iniPath: File) extends Player with Runnable {
   var BLOCK_SIZE = 4096
   var SAMPLE_RATE = 44100
@@ -29,26 +35,45 @@ class VstPlayer(openAL: OpenAL, iniPath: File) extends Player with Runnable {
     var candidates: List[File] = List(
       new File("""C:\Program Files (x86)\VstPlugins"""),
       new File("""C:\Program Files\Cakewalk\VstPlugins"""))
-    val candidate: File = candidates(0)
-    return new File(candidate, vstName)
+    for (candidate <- candidates) {
+      if (candidate.exists()) {
+        return new File(candidate, vstName)
+      }
+    }
+    return new File(vstName)
+  }
+  
+  def loadPitchList(pitchList: String): List[Int] = {
+    val pitches = pitchList.split(",")
+    val result = new ListBuffer[Int];
+    for (pitchStr <- pitches) {
+      result += pitchStr.trim().toInt
+    }
+    return result.toList;
   }
 
-  LibraryLoader.loadEmbededLibrary("jvsthost2.dll")
+  if (!VstPlayer.initialized) {
+    LibraryLoader.loadEmbededLibrary("jvsthost2.dll")
+    VstPlayer.initialized = true
+  }
+
   val vstPath = findVstPath(ini.getProperty("plugin"))
   val vst = JVstHost2.newInstance(vstPath, SAMPLE_RATE, BLOCK_SIZE)
+  vst.turnOn()
+  vst.openEditor("VSTEDITOR")
   JVstPersistence.ignorePluginVersion = true
   val presetName = ini.getProperty("preset")
   if (presetName != null) {
     JVstPersistence.loadPreset(vst, new File(vstPath.getParent(), presetName))
   }
-  vst.turnOn()
+  val basePitch = ini.getProperty("basePitch", "69").toInt
   val audioThread = new VSTClip(vst, openAL.createSource());
   var playing: Boolean = false;
 
   def start() = playing = true
   def stop() = playing = false
 
-  val harmonic = List(0, 2, 3, 5, 7, 10)
+  val harmonic = loadPitchList(ini.getProperty("pitchList", "0,2,3,5,7,10"))
   var prevIndexXY = 0f
 
   val thread = new Thread(audioThread);
@@ -62,6 +87,8 @@ class VstPlayer(openAL: OpenAL, iniPath: File) extends Player with Runnable {
   vst.setParameter(NexusControlMapping.mstFlt, 1)
   vst.setParameter(NexusControlMapping.revMix, 80 / 127f)
   vst.setParameter(NexusControlMapping.output, 0.2f)
+  
+  vst.setTempo(120)
 
   def run() {
     try {
@@ -72,7 +99,7 @@ class VstPlayer(openAL: OpenAL, iniPath: File) extends Player with Runnable {
         } else {
           prevIndexXY = xy
           val index = (this.y * 11).intValue()
-          val note = (harmonic(index % harmonic.length) + 12 * (index / harmonic.length) + 69).asInstanceOf[Int];
+          val note = (harmonic(index % harmonic.length) + 12 * (index / harmonic.length) + basePitch).asInstanceOf[Int];
 
           vst.setParameter(NexusControlMapping.mstCut, 0.4f + this.x * 0.6f);
 
